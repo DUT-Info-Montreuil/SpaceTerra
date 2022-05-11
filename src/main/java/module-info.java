be42edfa6568104611082fe4ff117032f18dcfe3
Armand <95768839+Armandpro12@users.noreply.github.com>
2022-05-11 12:48:06 +0200

    json library

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module les_gens.spaceterra {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires com.google.gson;
 
     opens les_gens.spaceterra to javafx.fxml;
     exports les_gens.spaceterra;
