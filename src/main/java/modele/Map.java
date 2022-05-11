b1f7358a0c2854fccfa91c838271231d3c8bf506
Tarook <tarook.zinou@gmail.com>
2022-05-11 22:21:23 +0200

    loop des layers dans constructeur de Map

--- src/main/java/modele/Map.java
+++ src/main/java/modele/Map.java
@@ -18,6 +18,7 @@ public class Map {
 
     public Map(String mapPath){
         this.map = loadMap(mapPath);
+        parseLayers(this.map);
     }
 
     public JSONObject loadMap(String mapPath){
@@ -31,6 +32,7 @@ public class Map {
 
         JSONObject map = (JSONObject) obj;
         System.out.println(map);
+        return map;
     } catch (
     FileNotFoundException e) {
         e.printStackTrace();
@@ -39,22 +41,30 @@ public class Map {
     } catch (ParseException e) {
         e.printStackTrace();
     }
-    return map;
+    return null;
 }
 
 
 
-    private static void parseMap(JSONObject map) {
+    private static void parseLayers(JSONObject map) {
         //Get employee object within list
         JSONArray layersData = (JSONArray) map.get("layers");
         JSONObject layer = (JSONObject)layersData.get(0); //gives data of layer 0
         JSONArray layerData = (JSONArray) layer.get("data");
         System.out.println(layerData.get(1));
 
+        Iterator<JSONObject> layersIterator =  layersData.iterator();
+
         ArrayList<Integer> data = new ArrayList<>();
+        while(layersIterator.hasNext()){
+            JSONObject currentLayer = layersIterator.next();
+            System.out.println("GOT LAYER ONE BOYS");
+
+        }
 
         for (int j = 0; j < data.size(); j++) {
             data.add((Integer) layerData.get(j));
+            System.out.println("for loop");
         }
     }
 }
