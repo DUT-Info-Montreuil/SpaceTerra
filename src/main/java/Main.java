1aaf5f9cfbe41abbd4987d9cfa7223574c0f27bd
Tarook <tarook.zinou@gmail.com>
2022-05-12 00:06:28 +0200

    Ajout d'une fenetre JavaFX vide pour tester

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import modele.Map;
 
@@ -6,5 +9,11 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Map map = new Map("src/main/resources/Map/Test.json");
+
+        Group root = new Group();
+        Scene scene = new Scene(root, 600,600, Color.LIGHTSKYBLUE);
+
+        stage.setScene(scene);
+        stage.show();
     }
 }
