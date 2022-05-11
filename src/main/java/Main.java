e081c81e2f1358a24d13b30530b535d532883a3c
Tarook <tarook.zinou@gmail.com>
2022-05-11 20:09:07 +0200

    Renommage fini fonctionnel

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
+import modele.Map;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-
+        Map map = new Map();
+        map.loadMap();
     }
 }
