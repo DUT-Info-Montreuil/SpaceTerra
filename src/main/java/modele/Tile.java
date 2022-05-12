0665523e69f3788d465dcbbe81079ca6c54b85e3
Tarook <tarook.zinou@gmail.com>
2022-05-12 23:26:22 +0200

    Class Tile compilable. A tester

--- src/main/java/modele/Tile.java
+++ src/main/java/modele/Tile.java
@@ -3,12 +3,24 @@ package modele;
 import javafx.scene.image.Image;
 import org.json.simple.JSONObject;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Tile {
     private int id;
     private Image image;
     private int tileWidth, tileHeight;
 
     public Tile(JSONObject tile){
+        id = (((Long)tile.get("id")).intValue()) + 1; // On ajoute 1 parce que les id commence a 0 mais 0 = vide dans le data des layers
+        tileWidth = ((Long)tile.get("imagewidth")).intValue();
+        tileHeight = ((Long)tile.get("imageheight")).intValue();
+        image = new Image(String.valueOf(getClass().getResource("/Sprites/TileSet/" + findTileFileName(tile))));
+    }
 
+    //((String) tile.get("image")).replace("\\", "")
+    public String findTileFileName(JSONObject tile){
+        Path p = Paths.get(((String) tile.get("image")).replace("\\", ""));
+        return p.getFileName().toString();
     }
 }
