bc56d3b5172aaa1fe8c8c905e821e53a9b727dbe
Tarook <tarook.zinou@gmail.com>
2022-05-14 00:55:46 +0200

    Ajout de fichiers de nouvelle map

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.ParallelCamera;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
@@ -12,9 +13,10 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception {
         Pane root = FXMLLoader.load(getClass().getResource("vueMap.fxml"));
         Scene scene = new Scene(root, 32*10,32*10, Color.LIGHTSKYBLUE);
+        ParallelCamera camera = new ParallelCamera();
+        scene.setCamera(camera);
         stage.setScene(scene);
         stage.show();
         root.requestFocus();
-
     }
 }
