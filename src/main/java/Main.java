af2db07c7fb3cb5394e5d64d19f609da128e7cdc
Tarook <tarook.zinou@gmail.com>
2022-05-14 13:21:03 +0200

    Swap from parallel cam to GameCam, not usable yet

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,13 +7,14 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import controleur.KeyHandler;
+import vue.GameCam;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Pane root = FXMLLoader.load(getClass().getResource("vueMap.fxml"));
         Scene scene = new Scene(root, 500,500, Color.DARKBLUE);
-        ParallelCamera camera = new ParallelCamera();
+        GameCam camera = new GameCam();
         scene.setCamera(camera);
         stage.setScene(scene);
         stage.show();
