c3bd2a70abc84e38b5569d0f88e3975a3a1feb0b
Armand <95768839+Armandpro12@users.noreply.github.com>
2022-05-17 10:08:28 +0200

    maintenant on voit le carré de suppression sur les blocks non solide

--- src/main/java/controleur/Controleur.java
+++ src/main/java/controleur/Controleur.java
@@ -137,6 +137,10 @@ public class Controleur implements Initializable {
         ArrayList<Block> deletedBlocks = new ArrayList<>();
                 for (Block b : terrain.getBlocks()) {
                     if (mouseHandler.getMouseX() < b.getHitX()+b.getTile().getHitbox().getWidth() && mouseHandler.getMouseX() > b.getHitX() && mouseHandler.getMouseY() < b.getHitY()+b.getTile().getHitbox().getHeight() && mouseHandler.getMouseY() > b.getHitY()) {
+                        Rectangle r = new Rectangle(b.getHitX(), b.getHitY(), b.getTile().getHitbox().getWidth(), b.getTile().getHitbox().getHeight());
+                        r.setFill(Color.TRANSPARENT);
+                        r.setStroke(Color.BLACK);
+                        panneauDeJeu.getChildren().add(r);
                         deletedBlocks.add(b);
                         System.out.println(b);
                         System.out.println("oui3");
@@ -146,10 +150,6 @@ public class Controleur implements Initializable {
         terrain.deleteBlock(deletedBlocks);
                 for (Block b : terrain.getSolidBlocks()) {
                     if (mouseHandler.getMouseX() < b.getHitX()+b.getTile().getHitbox().getWidth() && mouseHandler.getMouseX() > b.getHitX() && mouseHandler.getMouseY() < b.getHitY()+b.getTile().getHitbox().getHeight() && mouseHandler.getMouseY() > b.getHitY()) {
-                        Rectangle r = new Rectangle(b.getHitX(), b.getHitY(), b.getTile().getHitbox().getWidth(), b.getTile().getHitbox().getHeight());
-                        r.setFill(Color.TRANSPARENT);
-                        r.setStroke(Color.BLACK);
-                        panneauDeJeu.getChildren().add(r);
                         deletedBlocks.add(b);
                         System.out.println("oui4");
                     }
