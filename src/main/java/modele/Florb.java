9abfbbbb5d8176762315c55242cc342ee483e521
Tarook <tarook.zinou@gmail.com>
2022-06-19 03:22:08 +0200

    Fix class Florb

--- src/main/java/modele/Florb.java
+++ src/main/java/modele/Florb.java
@@ -9,40 +9,37 @@ public class Florb extends Enemy{
 
 
     public Florb(int x, int y, Terrain terrain) {
-        super(10, 6, new Hitbox(22,16,x,y, false), 200, terrain, new ArrayList<String>(){
+        super(10, 1, new Hitbox(22, 16, x, y, false), 12, 3, terrain, 0, true, new ArrayList<String>(){
             {
                 add("idle");
             }
         });
-        this.strength = 3;
-        //super(10, 1, new Hitbox(22, 16, x, y), "/Sprites/Enemies/Florb/Florb.gif", 12, 3, terrain, 0, true);
-        this.setFlying(true);
     }
 
 
     @Override
     public void action() {
-        Block b = Controleur.terrain.getBlock(getHitbox().getX().intValue(), getHitbox().getY().intValue()+350);
-        if (b != null && b.getTile().getHitbox().isSolid()) {
+        Block b = Controleur.terrain.getBlock((int)getHitbox().getX(), (int)getHitbox().getY() + 350);
+        if (b != null && b.getHitbox().isSolid()) {
             moveY(-1);
         }
 
     }
 
     public void moveY(int vertDirection) {
-        Block b = Controleur.terrain.getBlock(this.getHitbox().getX().intValue(), this.getHitbox().getY().intValue() + this.getSpeed());
-        if(b == null || !b.getTile().getHitbox().isSolid()){
-            this.getHitbox().setY(this.getHitbox().getY().intValue() + this.getSpeed() * vertDirection);
+        Block b = Controleur.terrain.getBlock((int)this.getHitbox().getX(), (int)this.getHitbox().getY() + this.getSpeed());
+        if(b == null || !b.getHitbox().isSolid()){
+            this.getHitbox().setY(this.getHitbox().getY()+ this.getSpeed() * vertDirection);
         }
         else {
-            this.getHitbox().setY(this.getHitbox().getY().intValue() - this.getSpeed());
+            this.getHitbox().setY(this.getHitbox().getY() - this.getSpeed());
         }
     }
 
 
     public void huntingY() {
-        Block b = Controleur.terrain.getBlock(getHitbox().getX().intValue(), getHitbox().getY().intValue()+200);
-        if (b != null && b.getTile().getHitbox().isSolid()) {
+        Block b = Controleur.terrain.getBlock((int)getHitbox().getX(), (int)getHitbox().getY() + 200);
+        if (b != null && b.getHitbox().isSolid()) {
             moveY(-1);
         }
         else {
@@ -53,20 +50,20 @@ public class Florb extends Enemy{
     @Override
     public void attack() {
         this.setSpeed(2);
-        if (this.getHitbox().getX().intValue() < Controleur.player.getHitbox().getX().intValue()) {
+        if (this.getHitbox().getX() < Controleur.player.getHitbox().getX()) {
             this.setIdleDirection(1);
-        } else if (this.getHitbox().getX().intValue() > Controleur.player.getHitbox().getX().intValue()) {
+        } else if (this.getHitbox().getX() > Controleur.player.getHitbox().getX()) {
             this.setIdleDirection(-1);
         }
         if(this.getAttackCooldown() > 0 && isCanAttack()){
             this.moveY(1);
             moveX(getIdleDirection());
             setAttackCooldown(getAttackCooldown() - 7);
-            if ((distanceToPosition(Controleur.player.getHitbox().getX().intValue(), Controleur.player.getHitbox().getY().intValue()) == 0 && !Controleur.player.isInvicible())) {
+            if ((distanceToPosition((int)Controleur.player.getHitbox().getX(), (int)Controleur.player.getHitbox().getY()) == 0 && !Controleur.player.isInvicible())) {
                 Controleur.player.decreaseHealth(2);
                 this.setAttackCooldown(50);
             }
-            else if(this.getHitbox().getY().intValue() == Controleur.player.getHitbox().getY().intValue()){
+            else if(this.getHitbox().getY() == Controleur.player.getHitbox().getY()){
                 this.setAttackCooldown(0);
             }
             Controleur.player.launchInvicibleCooldown();
